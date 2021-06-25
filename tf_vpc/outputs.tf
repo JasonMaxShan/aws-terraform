fda5a3e77026a3ff779ab8efced6fd2ef28aff64
JasonMaxShan <1028szpg@gmail.com>
2021-06-25 11:28:04 +0800

    Update outputs.tf

--- tf_vpc/outputs.tf
+++ tf_vpc/outputs.tf
@@ -13,3 +13,9 @@ output "database_subnet_group_name" {
   description = "Name of database subnet group"
   value	      = module.vpc.database_subnet_group_name
 }
+output "public_subnets" {
+  value       = module.vpc.public_subnets
+}
+output "elb_security_group_id"{
+  value       = module.http_sg.security_group_id
+}
